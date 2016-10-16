12e609851d9d54bac1499a4dddb1e2bd833703aa
kyh8@duke.edu <kyh8@duke.edu>
2016-10-15 20:02:48 -0400

    Initial react framework setup, sidebar, routing, and tabs

---
+++ src/main/webapp/js/Problems.js
@@ -0,0 +1,56 @@
+const React = require('react');
+const problems = [
+  {
+    number: 1,
+    points: 10,
+    difficulty: 'Hard',
+    title: 'Longest Palindromic Subsequence'
+  },
+  {
+    number: 2,
+    points: 5,
+    difficulty: 'Medium',
+    title: 'Knapsack Problem'
+  },
+  {
+    number: 3,
+    points: 5,
+    difficulty: 'Easy',
+    title: 'My Google Inteview Question'
+  }
+]
+
+export class Problems extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  renderProblemPreviews() {
+    let elements = [];
+    problems.forEach((problem, index) => {
+      const problemElement = (
+        <div key={"problem-" + index} className="problem-preview card">
+          <div className="problem-number-container">
+            <div className="problem-number">{problem.number}</div>
+          </div>
+          <div className="problem-description">
+            <div className="problem-title">{problem.title}</div>
+            <div className="problem-subtitle">{problem.points} points • Difficulty: {problem.difficulty}</div>
+          </div>
+        </div>
+      )
+      elements.push(problemElement);
+    });
+    return elements;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderProblemPreviews()}
+      </div>
+    );
+  }
+}
+
+module.exports = Problems;
---
+++ src/main/webapp/js/Registration.js
@@ -0,0 +1,105 @@
+const React = require('react');
+const TITLE_HEIGHT = 100;
+const CONTAINER_WIDTH = 500;
+const CONTAINER_HEIGHT = 370;
+
+export class Registration extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log('init');
+    this.state = {
+      error: false
+    }
+  }
+
+  submitLoginInfo(e) {
+    e.preventDefault();
+    console.log('submitted');
+    const emailField = document.getElementById('email-login-field');
+    const passwordField = document.getElementById('password-login-field');
+    const email = emailField.value;
+    const password = passwordField.value;
+    console.log(email, ',', password);
+    if (email.length == 0 || password.length == 0) {
+      console.log('error!');
+    }
+  }
+
+  render() {
+    const width = this.props.dimensions.width - 160;
+    const height = this.props.dimensions.height - 100;
+
+    return (
+      <div className="registration-tab">
+        <div
+          className="registration card"
+          style={{
+            left: '50px',
+            top: '50px',
+            width: width,
+            height: height
+          }}>
+          <div className="tab-title">
+            Registration
+          </div>
+          <div
+            className="registration-form-container"
+            style={{
+              left: (width - CONTAINER_WIDTH)/2 + 'px',
+              top: (height - CONTAINER_HEIGHT - TITLE_HEIGHT)/2 + TITLE_HEIGHT + 'px',
+              width: CONTAINER_WIDTH + 'px',
+              height: CONTAINER_HEIGHT + 'px',
+              borderRadius: '10px',
+              boxShadow: '0 0 2px black'
+            }}>
+            <div style={{
+              width: '300px',
+              position: 'absolute',
+              left: (CONTAINER_WIDTH - 320)/2 + 'px',
+              top: '20px'
+            }}>
+              <div className="registration-form-login unselectable">
+                Registered User?
+              </div>
+              <form onSubmit={(e) => this.submitLoginInfo(e)}>
+                <input type="text" id="email-login-field" className="text-field" placeholder="Email"/>
+                <input
+                  type="password"
+                  id="password-login-field"
+                  className="text-field"
+                  placeholder="Password"
+                  autoComplete="new-password"/>
+                <input type="submit" style={{display: 'none'}}/>
+              </form>
+              <div className="link-button login" onClick={this.submitLoginInfo.bind(this)}>Login</div>
+              <div style={{
+                width: '200px',
+                marginLeft: '50px',
+                marginTop: '40px'
+              }}>
+                <hr className="registration-divider"/>
+                <div style={{
+                  backgroundColor: 'white',
+                  textAlign: 'center',
+                  width: '50px',
+                  position: 'relative',
+                  top: '-20px',
+                  left: '75px'
+                }}>
+                  OR
+                </div>
+              </div>
+              <div
+                className="link-button register registration-form-sign-up">
+                <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                &nbsp; Sign Up
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Registration;
